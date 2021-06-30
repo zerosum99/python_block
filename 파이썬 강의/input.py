6af306d3fd901811a09b620b7eac3061aa86e926
Moon Yong Joon <myjlms99@gmail.com>
2021-06-30 13:41:22 +0900

    9999900012345

---
+++ 파이썬 강의/input.py
@@ -0,0 +1,8 @@
+
+if __name__ == "__main__" : 
+    
+    i1 = int(input(" 첫번째 숫자를 입력하세요 "))
+    i2 = int(input(" 두번째 숫자를 입력하세요 "))
+    print("-" * 50)
+    for i in range(i1,i2) :
+        print(i)
